11458ce7d2657bb17421d542c3feec84ea81a8fe
projectflutrend <bitowaqr@gmail.com>
2019-10-24 17:31:07 +0100

    savegame 1

--- R/src1_data_proc.R
+++ R/src1_data_proc.R
@@ -4,6 +4,7 @@
 
 library(sp)
 library(geosphere)
+library(dplyr)
 
 
 # LSOA centroids
@@ -26,28 +27,37 @@ lsoa_participation = read.csv("./raw_data/parkrun_data/runs_per_lsoa_010117_1012
 # LSOA total population
 lsoa_pop = read.csv("./raw_data/IoD2019_Population_Denominators.csv",stringsAsFactors = F)
 lsoa_pop[,-c(1:4)] = data.frame(apply(lsoa_pop[,-c(1:4)],2,function(x){as.numeric(as.character(gsub(",","",x)))}),stringsAsFactors = F)
-lsoa_pop = lsoa_pop[,c(1,5)]
-names(lsoa_pop) = c("code","total_pop")
+lsoa_pop = lsoa_pop[,c(1,5,7)]
+names(lsoa_pop) = c("code","total_pop","perc_non_working_age")
+lsoa_pop$perc_non_working_age = 1-(lsoa_pop$perc_non_working_age / lsoa_pop$total_pop)
+
+# density
+lsoa_density = read.csv("./raw_data/Mid-2017 Population Density.csv",stringsAsFactors = F)[,c(1,4)]
+lsoa_density = rename(lsoa_density, code = Code)
+lsoa_density = merge(lsoa_density,lsoa_pop[,1:2],by="code")
+lsoa_density$pop_density = round(lsoa_density$total_pop / lsoa_density$Area.Sq.Km,2)
+lsoa_density = lsoa_density[,c(1,4)]
 
 # LSOA IMD score
 lsoa_imd = read.csv("./raw_data/IoD2019_Scores.csv", stringsAsFactors = F)
 lsoa_imd = lsoa_imd[,-c(2:4,13:20)]
-names(lsoa_imd)[1] = "code"
+names(lsoa_imd) = c("code","imd","d_income","d_employment","d_education","d_health","d_crime","d_housing","d_enviroment")
 
 
 # ethnicity
 lsoa_ethnicity = read.csv("raw_data/LSOA_Ethnicity.csv",stringsAsFactors = F)
 lsoa_ethnicity = lsoa_ethnicity[,3:5]
 lsoa_ethnicity = data.frame(code = lsoa_ethnicity$geography.code,
-                            perc_white = lsoa_ethnicity$Sex..All.persons..Age..All.categories..Age..Ethnic.Group..White..Total..measures..Value/lsoa_ethnicity$Sex..All.persons..Age..All.categories..Age..Ethnic.Group..All.categories..Ethnic.group..measures..Value)
+                            perc_bme = 1-lsoa_ethnicity$Sex..All.persons..Age..All.categories..Age..Ethnic.Group..White..Total..measures..Value/lsoa_ethnicity$Sex..All.persons..Age..All.categories..Age..Ethnic.Group..All.categories..Ethnic.group..measures..Value)
 lsoa_ethnicity = lsoa_ethnicity[!(grepl("W",lsoa_ethnicity$code)),]                            
 # density?
 
 # merge everything
-lsoa_df = Reduce(function(x, y) merge(x, y,by="code", all=TRUE), list(lsoa_participation,lsoa_distance, lsoa_imd, lsoa_pop,lsoa_ethnicity))
+lsoa_df = Reduce(function(x, y) merge(x, y,by="code", all=TRUE), list(lsoa_participation,lsoa_distance, lsoa_imd, lsoa_pop,lsoa_density,lsoa_ethnicity))
 lsoa_df$run_count[is.na(lsoa_df$run_count)] = 0
 # observation period 1 Jan 2018-10 Dec 2018
 obs_period = 49 # weeks
 
 
 write.csv(lsoa_df,"./output/lsoa_df.csv",row.names = F)
+
--- R/src2_analysis.R
+++ R/src2_analysis.R
@@ -1,1 +1,83 @@
+
+
 # analyiss 
+
+options(scipen = 99)
+# lsoa_df = read.csv("output/lsoa_df.csv",stringsAsFactors = F)
+
+# explore correlations
+library(corrplot)
+library(ggplot2)
+cor_mat = cor(lsoa_df[,-1])
+rownames(cor_mat) = colnames(cor_mat) = substr(colnames(cor_mat),1,20)
+corrplot(cor_mat,addCoef.col = "black")
+
+
+melted <- reshape2::melt(lsoa_df, id.vars=c("code", "run_count"))
+
+p <- ggplot(melted, aes(value, run_count)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~variable,ncol = 3,scales = "free")
+p
+
+
+
+
+model2 = glm(run_count ~ imd + pop_density+mn_dstn + perc_working+perc_bme,
+             data = lsoa_df,
+             family = poisson(link="log"),
+             offset = log(total_pop))
+summary(model2)
+x = summary(model2)
+1-((x$deviance-length(coef(x)[,1]))/x$null.deviance)
+
+scaled_df = data.frame(code = lsoa_df$code,
+                       run_count = lsoa_df$run_count,
+                       scale(lsoa_df[,-c(1,2,12)]),total_pop = lsoa_df$total_pop,stringsAsFactors = F)
+model3 = glm(run_count ~ d_income + d_employment + d_education + d_health + d_crime + d_housing + d_enviroment + mn_dstn + perc_non_working_age + log(pop_density)   + perc_bme,
+             data = scaled_df,
+             family = poisson(link="log"),
+             offset = log(total_pop))
+summary(model3)
+x = summary(model3)
+1-((x$deviance-length(coef(x)[,1]))/x$null.deviance)
+
+
+model3.2 = glm(run_count ~ imd *perc_bme+ mn_dstn,
+             data = scaled_df,
+             family = poisson(link="log"),
+             offset = log(total_pop))
+x = summary(model3.2)
+r2.2 = 1-((x$deviance-length(coef(x)[,1]))/x$null.deviance)
+  
+  
+ggplot(lsoa_df, aes( x=imd,y=run_count)) +
+  geom_point() +
+  geom_smooth()
+  
+    
+
+
+
+
+model4 = glm(run_count ~ imd + mn_dstn + perc_bme,
+             data = scaled_df,
+             family = poisson(link="log"),
+             offset = log(total_pop))
+summary(model4)
+
+
+ggplot(lsoa_df,aes(x=perc_bme,y=run_count)) +
+  geom_point() +
+  geom_smooth()
+
+ggplot(lsoa_df,aes(x=(perc_bme),y=log(run_count))) +
+  geom_point() +
+  geom_smooth()
+
+
+
+
+lsoa_cntrds$name[lsoa_cntrds$code == "E06000031"]
+
